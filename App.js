70610ab6cc5515b9175a552a9711b86c5ce9e0f5
ambadasgote7 <ambadasgote7@gmail.com>
2026-01-12 08:16:15 +0530

    Ep02 - Igniting our App

---
+++ App.js
@@ -0,0 +1,44 @@
+
+import React from "react";
+import {ReactDOM, createRoot} from "react-dom/client";
+
+const parent = React.createElement(
+    "div",
+    {id : "parent"},
+    [React.createElement(
+        "div",
+        {id : "child"},
+        [React.createElement("h1", {}, "I am H1 tag"), React.createElement("h2", {}, "I am H2 tag")]
+    ), 
+    React.createElement(
+        "div",
+        {id : "child2"},
+        [React.createElement("h1", {}, "I am H1 tag"), React.createElement("h2", {}, "I am H2 tag")]
+    )]
+);
+
+// const parent = React.createElement(
+//     "div",
+//     {id : "parent"},
+//     React.createElement(
+//         "div",
+//         {id : "child"},
+//         React.createElement("h1", {}, "I am H1 tag")
+//     )
+// );   
+
+
+// const parent = React.createElement(
+//     "div",
+//     {id : "parent"},
+//     React.createElement(
+//         "div",
+//         {id : "child"},
+//         [React.createElement("h1", {}, "I am H1 tag"), React.createElement("h2", {}, "I am H2 tag")]
+//     )
+// );
+
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(parent);
